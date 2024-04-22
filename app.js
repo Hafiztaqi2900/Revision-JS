451185334054aab5f3d3a7eaf09150538b6dc442
Hafiz Muhammad Taqi <hafiztaqi1829@gmail.com>
2024-04-22 11:39:50 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// var name = "taqi"
+// console.log(name)
+// name = "Taqi Amin"
+// console.log(name)
+
+// var height = 5.7
+// console.log(height)
+
+// alert(5 + 4 + 5)
+//  alert(5 + "4" + 5)
+
