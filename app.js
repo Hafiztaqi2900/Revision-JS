2955d6f1cb438586b7a7cab38875a3f1736324b8
Hafiz Muhammad Taqi <hafiztaqi1829@gmail.com>
2024-11-13 23:46:18 +0500

    Revision

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-// var name = "taqi"
-// console.log(name)
-// name = "Taqi Amin"
-// console.log(name)
+// console.log('Hello World') 47
+// var num1=203;
+// var num2=218;
 
-// var height = 5.7
-// console.log(height)
+// console.log("The value of num1 + num2 is = " + num1+num2) 48
 
-// alert(5 + 4 + 5)
-//  alert(5 + "4" + 5)
- var playerinfo = "Amir" + 5
- alert(playerinfo) 
+// var Name='Taqi';
+// var Fname='Amin';
+// var age=17;
+// var full=(Name+Fname+age)
+// console.log(full)
+// var desc=`My name is ${Name} also my father ${Fname}`
+// console.log(desc)
+
+// console.log('Hello My World \n My name is \t Taqi')
+
