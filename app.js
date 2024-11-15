39ad3a5439661729bde88bb34405be3db8b3c9df
Hafiz Muhammad Taqi <hafiztaqi1829@gmail.com>
2024-11-15 11:01:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// console.log('Hello World') 48
+// Class#48
+// console.log('Hello World') 
 // var num1=203;
 // var num2=218;
+// console.log("The value of num1 + num2 is = " + num1+num2) 
 
-// console.log("The value of num1 + num2 is = " + num1+num2) 48
-
+// Class#49
 // var Name='Taqi';
 // var Fname='Amin';
 // var age=17;(
@@ -11,29 +12,31 @@
 // console.log(full)
 // var desc=`My name is ${Name} also my father ${Fname}`
 // console.log(desc)
-// console.log('Hello My World \n My name is \t Taqi')  49
+// console.log('Hello My World \n My name is \t Taqi')  
 
-// class 50 sae continue krna hae
-var str = "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-console.log(str)
-var position= str.indexOf('dolor')
-console.log(position)
-position=str.lastIndexOf('amet')
-console.log(position)
+// class#50 
+// var str = "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+// console.log(str)
+// var position= str.indexOf('dolor')
+// console.log(position)
+// position=str.lastIndexOf('amet')
+// console.log(position)
 
-var example='I love Pakistan'
-console.log(example)
-example1=example.slice(2,10)
-console.log(example1)
+// var example='I love Pakistan'
+// console.log(example)
+// example1=example.slice(2,10)
+// console.log(example1)
 // Also we can use substring and substr
 
-var replaced=example.replace('Pakistan','Cricket team of Pakistan');
-console.log(example)
-console.log(replaced)
+// var replaced=example.replace('Pakistan','Cricket team of Pakistan');
+// console.log(example)
+// console.log(replaced)
 //  () use krna is permenant in below condition
-console.log(replaced.toUpperCase())
-console.log(replaced.toLowerCase())
+// console.log(replaced.toUpperCase())
+// console.log(replaced.toLowerCase())
 
-var string3=replaced.concat(' Also I am A Pakistani')
-console.log(string3)
+// var string3=replaced.concat(' Also I am A Pakistani')
+// console.log(string3)
 // charAt is used for data konsae number pr hae 
+
+// Class#51 Continue
