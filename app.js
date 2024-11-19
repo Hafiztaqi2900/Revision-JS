60271960e1017ddd8a01a4660550299d13855a44
Hafiz Muhammad Taqi <hafiztaqi1829@gmail.com>
2024-11-19 11:53:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -78,25 +78,25 @@
 // console.log(cities)
 
 // Class#53
-var name='Taqi'
-var name1='Naqi'
-var name2='Abiha'
-var remarks=' Is a good student'
-var abc='Good Morning'
+// var name='Taqi'
+// var name1='Naqi'
+// var name2='Abiha'
+// var remarks=' Is a good student'
+// var abc='Good Morning'
 
-function greet(name, remarks ,abc) {
-    console.log(abc + " " + name)
-    console.log(name + remarks)
-}
+// function greet(name, remarks ,abc) {
+//     console.log(abc + " " + name)
+//     console.log(name + remarks)
+// }
 
-greet(name, remarks, abc)
-greet(name1, remarks, abc)
-greet(name2, remarks, abc)
+// greet(name, remarks, abc)
+// greet(name1, remarks, abc)
+// greet(name2, remarks, abc)
 
-function sum(a,b,c) {
-    return(
-        d = a+b+c 
-    )
-}
-var returnvalue=sum(1,2,3)
-console.log(returnvalue)
+// function sum(a,b,c) {
+//     return(
+//         d = a+b+c 
+//     )
+// }
+// var returnvalue=sum(1,2,3)
+// console.log(returnvalue)
